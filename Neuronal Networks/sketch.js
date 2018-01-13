279c097723e2f8143237f5e805de7c37d6bf92fa
JonasFovea <30690893+JonasFovea@users.noreply.github.com>
2018-01-13 21:56:58 +0100

    Drawing Points

--- Neuronal Networks/sketch.js
+++ Neuronal Networks/sketch.js
@@ -1,11 +1,37 @@
 let p;
+let n = 100;
+let points = new Array(n);
+
+
+
+
+
 
 function setup(){
-  createCanvas(400,200);
+  createCanvas(700,500);
+  background(255);
+
+
   p = new Perceptron();
   let inputs = [-1,0.5];
   let guess = p.guess(inputs);
 
   console.log(guess);
+
+
+  for (var i = 0; i < points.length; i++) {
+    points[i] = new Point();
+  }
 }
-function draw(){}
+
+
+function draw(){
+  noLoop();
+  console.log(points);
+  //stroke(0);
+  //line(0,0,width,height);
+
+  for (var i = 0; i < points.length; i++) {
+    points[i].show();
+  }
+}
