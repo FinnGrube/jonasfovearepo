5765b19f797a9c974c0684ccee28aeff0002a0d8
JonasFovea <30690893+JonasFovea@users.noreply.github.com>
2018-01-13 21:36:01 +0100

    First Guessing

--- Neuronal Networks/Perceptron.js
+++ Neuronal Networks/Perceptron.js
@@ -1,33 +1,33 @@
+/*
+* Activation Function
+*/
+function sign(n){
+  if(n>=0){
+    return 1;
+  }
+  return -1;
+}
+
 class Perceptron{
 
-  /*
-  * Activation Function
-  */
-  sign(n){
-    if(n>=0){
-      return 1;
-    }
-    return -1;
-  }
-
-  this.weights=[];
-
   constructor(){
+    this.weights= new Array(2);
 
     for (let i = 0; i < this.weights.length; i++) {
       this.weights[i] = random(-1,1);
     }
-
   }
 
-  guess(inputs[]){
+
+
+
+  guess(inputs){
     let sum = 0;
     for (var i = 0; i < this.weights.length; i++) {
       sum += inputs[i] * this.weights[i];
     }
-
-    let output  = sign (sum);
-
+    let output  = sign(sum);
+    return output;
   }
 
 }
--- Neuronal Networks/sketch.js
+++ Neuronal Networks/sketch.js
@@ -3,7 +3,7 @@ let p;
 function setup(){
   createCanvas(400,200);
   p = new Perceptron();
-  let inputs = {-1,0.5};
+  let inputs = [-1,0.5];
   let guess = p.guess(inputs);
 
   console.log(guess);
