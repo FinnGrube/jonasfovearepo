71ca9cfd61c5f39cbb69e52dfdab333b1dbd35bb
JonasFovea <30690893+JonasFovea@users.noreply.github.com>
2018-01-13 22:28:17 +0100

    Training clickwise

--- Neuronal Networks/Perceptron.js
+++ Neuronal Networks/Perceptron.js
@@ -12,6 +12,8 @@ class Perceptron{
 
   constructor(){
     this.weights= new Array(2);
+    this.lr = 0.01;
+
 
     for (let i = 0; i < this.weights.length; i++) {
       this.weights[i] = random(-1,1);
@@ -28,6 +30,17 @@ class Perceptron{
     }
     let output  = sign(sum);
     return output;
+  }
+
+
+
+  train(inputs, target){
+    let gu = this.guess(inputs);
+    let err = target - gu;
+
+    for (var i = 0; i < this.weights.length; i++) {
+      this.weights[i] += err*inputs[i]*this.lr;
+    }
   }
 
 }
--- Neuronal Networks/sketch.js
+++ Neuronal Networks/sketch.js
@@ -1,4 +1,4 @@
-let p;
+let brain;
 let n = 100;
 let points = new Array(n);
 
@@ -8,13 +8,13 @@ let points = new Array(n);
 
 
 function setup(){
-  createCanvas(700,500);
+  createCanvas(700,700);
   background(255);
 
 
-  p = new Perceptron();
+  brain = new Perceptron();
   let inputs = [-1,0.5];
-  let guess = p.guess(inputs);
+  let guess = brain.guess(inputs);
 
   console.log(guess);
 
@@ -22,16 +22,43 @@ function setup(){
   for (var i = 0; i < points.length; i++) {
     points[i] = new Point();
   }
+  //console.log(points);
 }
 
 
 function draw(){
-  noLoop();
-  console.log(points);
-  //stroke(0);
-  //line(0,0,width,height);
+  //noLoop();
+
+  line(0,0,width,height);
 
   for (var i = 0; i < points.length; i++) {
     points[i].show();
+  }
+
+  for (let j = 0; j < points.length;j++) {
+    let pt = points[j];
+    //console.log(pt);
+    let inputs = [pt.x,pt.y];
+    let target = pt.label;
+    //brain.train(inputs,target);
+
+    let guess = brain.guess(inputs);
+    if(guess == target){
+      fill(0,255,0);
+    }else{
+      fill(255,0,0);
+    }
+
+    noStroke();
+    ellipse(pt.x,pt.y,5,5);
+  }
+}
+
+function mousePressed(){
+  for (let j = 0; j < points.length;j++) {
+    let pt = points[j];
+    let inputs = [pt.x,pt.y];
+    let target = pt.label;
+    brain.train(inputs,target);
   }
 }
